60b649bcccab276e14cfa319d4336d6599387f0b
Paul <paul@glemma.nl>
2015-02-06 14:40:05 -0500

    feat(tests/test_utils): Renamed SimpleTest to UtilsTest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,9 +7,6 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class UtilsTest(TestCase):
+    def test_send_push_notification(self):
+        pass
